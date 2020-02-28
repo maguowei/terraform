df8fa6bd450238fe7311bd5102d6cbab191242f2
maguowei <imaguowei@gmail.com>
2020-02-28 20:40:03 +0800

    add aws/lightsail

---
+++ aws/lightsail/README.md
@@ -0,0 +1,12 @@
+# AWS Lightsail
+
+```bash
+export AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION={AWS_DEFAULT_REGION}
+
+terraform init
+terraform apply
+
+terraform destroy
+```
---
+++ aws/lightsail/lightsail.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "ap-northeast-1"
+}
+
+resource "aws_lightsail_key_pair" "my_key_pair" {
+  name       = "my_key_pair"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_lightsail_instance" "ubuntu" {
+  name              = "ubuntu"
+  availability_zone = "ap-northeast-1a"
+  blueprint_id      = "ubuntu_18_04"
+  bundle_id         = "nano_2_0"
+  key_pair_name     = "my_key_pair"
+  user_data         = "curl -fsSL https://get.docker.com/ | sh; sudo usermod -aG docker ubuntu"
+  tags = {
+    app = "ubuntu"
+  }
+}
