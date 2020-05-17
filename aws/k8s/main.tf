2e0f31f9e7c16f9b3c40e25c08c909e9bf71e77a
maguowei <imaguowei@gmail.com>
2020-05-17 10:01:56 +0800

    k8s set multiple resource lightsail instances by count

--- aws/k8s/main.tf
+++ aws/k8s/main.tf
@@ -8,32 +8,10 @@ resource "aws_lightsail_key_pair" "my_key_pair" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_lightsail_instance" "k8s-1" {
-  name              = "k8s-1"
-  availability_zone = "ap-northeast-1a"
-  blueprint_id      = "ubuntu_18_04"
-  bundle_id         = "medium_2_0"
-  key_pair_name     = "my_key_pair"
-  user_data         = "curl -fsSL https://get.docker.com/ | sh; sudo usermod -aG docker ubuntu"
-  tags = {
-    app = "k8s"
-  }
-}
+resource "aws_lightsail_instance" "k8s" {
+  count = 4
 
-resource "aws_lightsail_instance" "k8s-2" {
-  name              = "k8s-2"
-  availability_zone = "ap-northeast-1a"
-  blueprint_id      = "ubuntu_18_04"
-  bundle_id         = "medium_2_0"
-  key_pair_name     = "my_key_pair"
-  user_data         = "curl -fsSL https://get.docker.com/ | sh; sudo usermod -aG docker ubuntu"
-  tags = {
-    app = "k8s"
-  }
-}
-
-resource "aws_lightsail_instance" "k8s-3" {
-  name              = "k8s-3"
+  name              = "k8s-${count.index}"
   availability_zone = "ap-northeast-1a"
   blueprint_id      = "ubuntu_18_04"
   bundle_id         = "medium_2_0"
